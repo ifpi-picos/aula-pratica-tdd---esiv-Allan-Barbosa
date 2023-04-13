a3e879f29a9f829c2f84a462f936af4811b6dbb6
Allan Barbosa <allanb3779@gmail.com>
2023-04-13 15:47:07 -0300

    quarto commit

--- Dollar.java
+++ Dollar.java
@@ -1,9 +1,16 @@
-public class Dollar extends Money {
-   Dollar(int amount) {
-      this.amount= amount;
-   }
+class Dollar extends Money {
 
-   Money times(int multiplier)  {
-    return new Dollar(amount * multiplier);
- }		
+    private String currency;
+
+    Dollar(int amount, String currency) {
+        super(amount, currency);
+    }
+
+    Money times(int multiplier) {
+        return Money.dollar(amount * multiplier);
+    }
+
+    String currency() {
+        return currency;
+    }
 }
--- MoneyTest.java
+++ MoneyTest.java
@@ -6,17 +6,22 @@ public class MoneyTest {
         assertTrue(Money.franc(5).equals(Money.franc(5)));
         assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-     }
+    }
 
-     public void testMultiplication() {
+    public void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
-     }
+    }
 
-     public void testFrancMultiplication() {
+    public void testFrancMultiplication() {
         Money five = Money.franc(5);
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
-     }
+    }
+
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
